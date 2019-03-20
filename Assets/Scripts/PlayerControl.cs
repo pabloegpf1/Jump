4b240139d0da5fd341a787007ab53f2164dd1b24
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-03-19 21:41:47 -0700

    Initial Commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,46 @@
+﻿/*  Character control script 
+    Created by: Pablo Escriva
+    March 2019
+*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour{
+
+    private Rigidbody rigidBody;
+    private Transform transform;
+    public float moveSpeed = 10.0f;
+    public float jumpSpeed = 250.0f;
+    public float rotateSpeed = 5;
+
+    private Boolean onAir;
+
+    void Start (){
+        rigidBody = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
+        onAir = false;
+    }
+
+    void Update(){
+        float xInput = Input.GetAxis ("Horizontal");
+        float zInput = Input.GetAxis ("Vertical");
+        float mouseX = Input.GetAxis("Mouse X") * rotateSpeed;
+                
+        Vector3 inputVector = new Vector3 ( xInput, 0.0f, zInput);
+
+        transform.Rotate(0, mouseX, 0);
+        rigidBody.AddRelativeForce (inputVector * moveSpeed);
+
+        if (Input.GetKeyDown ("space") && onAir == false){
+            rigidBody.AddForce(0, jumpSpeed, 0);
+            onAir = true;
+        } 
+
+    }
+
+    void OnCollisionStay(Collision other){
+        onAir = false;
+    }
+}
---
+++ Assets/Scripts/StartButtonClicked.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonClicked : MonoBehaviour{
+
+    public void start(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+}
