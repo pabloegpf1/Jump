68bf70daaa869ddebf6fb6d3dd6d2b57a69a0936
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-03-19 23:19:50 -0700

    Revert "Initial Commit"
    This reverts commit 4b240139d0da5fd341a787007ab53f2164dd1b24.

--- Assets/Scripts/StartButtonClicked.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class StartButtonClicked : MonoBehaviour{
-
-    public void start(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
-    }
-}
